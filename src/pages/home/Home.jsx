82f87adeadc587d2010863ea17cc98e386e0db12
capp16 <48810247+nonokiNeko@users.noreply.github.com>
2023-09-28 15:26:16 +0700

    Add files via upload

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,48 +20,48 @@ const Home = () => {
   const { userDetails,showLogin } = useContext(UserContext);
   console.log(showLogin,"00000000")
   let currentUser = userDetails && userDetails.userType ?  userDetails.userType.toLowerCase() : 'guest'
-  let widgetData=[
-    {
-      title: "No. of buses currently running",
-      isMoney: false,
-      isCount:true,
-      count: 34,
-      colorCode:true,
-      total:141,
-    },
-    {
-      title: "No. of supervisors",
-      isMoney: false,
-      isCount:true,
-      count: 11,
+  // let widgetData=[
+  //   {
+  //     title: "No. of buses currently running",
+  //     isMoney: false,
+  //     isCount:true,
+  //     count: 34,
+  //     colorCode:true,
+  //     total:141,
+  //   },
+  //   {
+  //     title: "No. of supervisors",
+  //     isMoney: false,
+  //     isCount:true,
+  //     count: 11,
       
-    },  {
-      title: "No. of aquifers depletion greather than 50%",
-      isMoney: false,
-      isCount:true,
-      count: 21,
-      colorCode:true,
-      total:141,
+  //   },  {
+  //     title: "No. of aquifers depletion greather than 50%",
+  //     isMoney: false,
+  //     isCount:true,
+  //     count: 21,
+  //     colorCode:true,
+  //     total:141,
       
-    },
-    {
-      title: "Generate survey",
-      isMoney: false,
-      isCount:true,
-      count: 98,
-      icon: (
-        <Link to="/survey">
-        <PollRoundedIcon
-          className="icon"
-          style={{
-            backgroundColor: "rgba(128, 0, 128, 0.2)",
-            color: "purple",
-          }}
-        />
-        </Link>
-      ),
-    }
-  ];;
+  //   },
+  //   {
+  //     title: "Generate survey",
+  //     isMoney: false,
+  //     isCount:true,
+  //     count: 98,
+  //     icon: (
+  //       <Link to="/survey">
+  //       <PollRoundedIcon
+  //         className="icon"
+  //         style={{
+  //           backgroundColor: "rgba(128, 0, 128, 0.2)",
+  //           color: "purple",
+  //         }}
+  //       />
+  //       </Link>
+  //     ),
+  //   }
+  // ];;
  
   return (
     <>
@@ -70,11 +70,11 @@ const Home = () => {
       
       <div className="homeContainer">
         <div><Header/></div> 
-        <div className="widgets">
+        {/* <div className="widgets">
           {widgetData.map((el)=>{
             return <Widget data={el} />
           })}
-        </div>
+        </div> */}
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Capacity Vs Month Graph)" aspect={2 / 1} />
